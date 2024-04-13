22d749330df65a746430e7516506b9ac04f96585
Joel Ferretti <166237203+FerrettiJoel@users.noreply.github.com>
2024-04-13 19:32:14 -0300

    Add files via upload

---
+++ controllers/controllers.js
@@ -0,0 +1,30 @@
+const getAllProducts = (req, res)=>({});
+
+const addproduct = (req, res)=>({});
+
+const editProduct = (req, res)=>({});
+
+const updateProduct = (req, res)=>({});
+
+const deleteProducts = (req, res)=>({});
+
+const addUser = (req, res)=>({});
+
+const editUser = (req, res)=>({});
+
+const updateUser = (req, res)=>({});
+
+const deleteUser = (req, res)=>({});
+//const getAllProducts = (req, res)=>({});
+
+export default {
+    getAllProducts,
+    addproduct,
+    editProduct,
+    updateProduct,
+    deleteProducts,
+    addUser,
+    editUser,
+    updateUser,
+    deleteUser
+}
